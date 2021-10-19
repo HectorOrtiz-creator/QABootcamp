c82e741ec9f82e9b25f009d564a75b5c599cbf24
Hector Alejandro Ortiz Rodriguez <hector.ortiz@hector.ortiz>
2021-10-19 16:41:02 -0500

    Deliverable1 - InProgress

--- POM/pages/LoginPage.js
+++ POM/pages/LoginPage.js
@@ -1,7 +1,11 @@
+import {Selector} from 'testcafe'
+
 class LoginPage{
     constructor(){
         this.usernameInput = Selector('#email')
+        this.passwordInput = Selector('#password')
+        this.loginButton = Selector('#login_form > button')
 
     }
 }
-export default new loginPage
+export default new LoginPage
--- POM/tests/Login.test.js
+++ POM/tests/Login.test.js
@@ -1,19 +1,19 @@
-import {Selector, t} from 'testcafe'
+import {t} from 'testcafe'
 import loginPage from '../pages/LoginPage'
 
 fixture("Login feature test")
-    .page("https://todoist.com/Users/login")
-    .beforeEach(async t => {
+    .page("https://todoist.com/users/showlogin")
+    /* .beforeEach(async => {
         await t.click('a[href="/users/showlogin"]')
-    })
+    }) */
 
 
 
-test(`As a user I should be able to log in using valid creds`, async => {
+test(`As a user I should be able to log in using valid creds`, async t => {
     await t
         .typeText(loginPage.usernameInput, 'hectorrtz16@gmail.com')
-        .typeText('#password', 'jaia1684')
-        .click('#login_form' > button)
+        .typeText(loginPage.passwordInput, 'jaia1684')
+        .click(loginPage.loginButton)
+        //assert 
 })
 
- 
