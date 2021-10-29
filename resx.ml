05f13e66ea9871e55b871b59310d4a13cf37ee38
Hector Alejandro Ortiz Rodriguez <hector.ortiz@hector.ortiz>
2021-10-29 16:07:43 -0500

    Final deliverable (updates to pakage file3 )

---
+++ resx.ml
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8" ?>
+<testsuite name="TestCafe Tests: Chrome 95.0.4638.54 / macOS 10.15.7" tests="1" failures="0" skipped="0" errors="0" time="13.944" timestamp="Fri, 29 Oct 2021 21:06:06 GMT" >
+  <testcase classname="Login feature test" name="As a user I should be able to log in using valid creds" time="13.935">
+  </testcase>
+</testsuite>
